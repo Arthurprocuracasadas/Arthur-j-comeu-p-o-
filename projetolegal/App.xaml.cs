7811a70848bd84f73158a89233b78f2f6d80b271
arthurmatheus <arthurmatheusfreitas@gmail.com>
2024-02-29 12:02:19 -0300

    Meu Primeiro Commit

---
+++ projetolegal/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace projetolegal;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ projetolegal/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace projetolegal;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
