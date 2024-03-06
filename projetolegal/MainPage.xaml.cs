c868126e80956645fbc7c1038779e2b13dd6bc63
arthur <arthurmatheusfreitas@gmail.com>
2024-03-06 09:41:50 -0300

    p/okok

--- projetolegal/MainPage.xaml.cs
+++ projetolegal/MainPage.xaml.cs
@@ -9,16 +9,36 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+  private void OnMenuStartButtonClicked(object sender, EventArgs args)
+  {
+    if (Application.Current != null)
+      Application.Current.MainPage = new GamePage();
+  }
+  
+  private void OnMenuSettingsButtonClicked(object sender, EventArgs args)
+  {
+    frameSettings.IsVisible = true;
+  }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+
+  private void OnMenuAboutButtonClicked(object sender, EventArgs args)
+  {
+    frameAbout.IsVisible = true;
+  }
+
+  private void OnDismissSettingsButtonClicked(object sender, EventArgs args)
+  {
+    frameSettings.IsVisible = false;
+  }
+
+  private void OnDismissAboutButtonClicked(object sender, EventArgs args)
+  {
+    frameAbout.IsVisible = false;
+  }
+
+  
 }
+
+
 
