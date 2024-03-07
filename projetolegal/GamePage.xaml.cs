260457cdd520247cb9ab66b161295c04ac79822c
arthur <arthurmatheusfreitas@gmail.com>
2024-03-07 11:27:27 -0300

    p/okok

---
+++ projetolegal/GamePage.xaml.cs
@@ -0,0 +1,19 @@
+namespace projetolegal;
+
+public partial class GamePage : ContentPage
+{
+  //------------------------------------------------------------------------------------------------
+
+  //List<HistoryStep> historia = new List<HistoryStep>();
+  //HistoryStep HistoryStepAtual;
+
+  //------------------------------------------------------------------------------------------------
+
+	public GamePage()
+	{
+		InitializeComponent();
+
+    //historia.Add(new HistoryStep()
+    
+    }
+}
--- projetolegal/MainPage.xaml.cs
+++ projetolegal/MainPage.xaml.cs
@@ -8,37 +8,40 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-
-  private void OnMenuStartButtonClicked(object sender, EventArgs args)
+ private void OnMenuStartButtonClicked(object sender, EventArgs args)
   {
     if (Application.Current != null)
       Application.Current.MainPage = new GamePage();
   }
+
+  //------------------------------------------------------------------------------------------------
   
   private void OnMenuSettingsButtonClicked(object sender, EventArgs args)
   {
     frameSettings.IsVisible = true;
   }
 
+  //------------------------------------------------------------------------------------------------
 
   private void OnMenuAboutButtonClicked(object sender, EventArgs args)
   {
     frameAbout.IsVisible = true;
   }
 
+  //------------------------------------------------------------------------------------------------
+
   private void OnDismissSettingsButtonClicked(object sender, EventArgs args)
   {
     frameSettings.IsVisible = false;
   }
+
+  //------------------------------------------------------------------------------------------------
 
   private void OnDismissAboutButtonClicked(object sender, EventArgs args)
   {
     frameAbout.IsVisible = false;
   }
 
-  
 }
-
 
 
