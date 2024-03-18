3b39b2a47327ea2e8e242a9320dc8574a62875c1
arthur <arthurmatheusfreitas@gmail.com>
2024-03-18 08:54:55 -0300

    p/oioi

--- projetolegal/GamePage.xaml.cs
+++ projetolegal/GamePage.xaml.cs
@@ -24,30 +24,69 @@ public partial class GamePage : ContentPage
       Id = 1,
       Texto = "o que você prefere fazer?",
       TemResposta=true,
-      TextoDaResposta01="bate na porta com delicadeza.",
-      TextoDaResposta02="da uma bicuda na porta com toda sua força.",
-      TextoDaResposta03="segue o caminho pela floresta.",
-      IdLevelResposta01=2,
-      IdLevelResposta02=3000,
-      IdLevelResposta03=3001
-    });
+      TextoResposta1="bate na porta com delicadeza.",
+      TextoResposta2="da uma bicuda na porta com toda sua força.",
+      TextoResposta3="segue o caminho pela floresta.",
+      IdLevelResposta1=2,
+      IdLevelResposta2=3000,
+      IdLevelResposta3=3001,
+      });
      historia.Add(new HistoriaStep()
     {
       Id = 2,
       Texto = "você é recebido por uma capivara humanoide bem vestida por sinal ( seu nome é Sr capivaldo)",
       TemResposta=false,
     });
+    historia.Add(new HistoriaStep()
+    {
+      Id = 3000,
+      Texto = "Sr capivaldo abre a porta com um machado e arranca sua cabeça",
+      TemResposta=false,
+      vcmorreu=true
+    });
+    historia.Add(new HistoriaStep()
+    {
+      Id = 3000,
+      Texto = "você caminha por algum tempo e acaba morrendo de fome",
+      TemResposta=false,
+      vcmorreu=true
+    });
     Iniciar();
   }
-     
+  //-------------------------------------------------------------------------------------------------------------------------------
      void Iniciar()
   {
     TrocaHistoriaStepAtual(0);
   }
+  //-------------------------------------------------------------------------------------------------------------------------------
     void PreencherPagina()
   {
     labelTexto.Text = HistoriaStepAtual.Texto;
+    
+    if (HistoriaStepAtual.vcmorreu)
+      Morte.IsVisible = true;
+    else
+      Morte.IsVisible = false;
+    
+    if (HistoriaStepAtual.TemResposta)
+    {
+      proximo.IsVisible = false;
+      Resposta1.IsVisible = true;
+      Resposta2.IsVisible = true;
+      Resposta3.IsVisible = true;
+      Resposta1.Text = HistoriaStepAtual.TextoResposta1;
+      Resposta2.Text = HistoriaStepAtual.TextoResposta2;
+      Resposta3.Text = HistoriaStepAtual.TextoResposta3;
+    }
+     else
+    {
+      proximo.IsVisible = true;
+      Resposta1.IsVisible = false;
+      Resposta2.IsVisible = false;
+      Resposta3.IsVisible = false;
+    }
   }
+  //------------------------------------------------------------------------------------------------------------------------
 void TrocaHistoriaStepAtual(int id)
   {
     HistoriaStepAtual = historia.Where(d => d.Id == id).First();
@@ -58,5 +97,28 @@ void ProximoClicked(object sender, EventArgs args)
     var proximoId = HistoriaStepAtual.Id + 1;
     TrocaHistoriaStepAtual(proximoId);
   }
-    
+//--------------------------------------------------------------------------------------------------------------------------    
+//------------------------------------------------------------------------------------------------
+
+  void Butao1(object sender, EventArgs args)
+  {
+    TrocaHistoriaStepAtual(HistoriaStepAtual.IdLevelResposta1);
+  }
+
+  //------------------------------------------------------------------------------------------------
+
+  void Butao2(object sender, EventArgs args)
+  {
+    TrocaHistoriaStepAtual(HistoriaStepAtual.IdLevelResposta2);
+  }
+
+  //------------------------------------------------------------------------------------------------
+
+  void Butao3(object sender, EventArgs args)
+  {
+    TrocaHistoriaStepAtual(HistoriaStepAtual.IdLevelResposta3);
+  }
+
+  //------------------------------------------------------------------------------------------------
+
 }
--- projetolegal/HistoryStep.cs
+++ projetolegal/HistoryStep.cs
@@ -5,11 +5,11 @@ public class HistoriaStep
   public bool TemImagem = false; 
   public string? UrlDaImagem; 
   public bool TemResposta = false; 
-  public string? TextoDaResposta01; 
-  public string? TextoDaResposta02; 
-  public string? TextoDaResposta03; 
-  public int IdLevelResposta01; 
-  public int IdLevelResposta02; 
-  public int IdLevelResposta03; 
+  public string? TextoResposta1; 
+  public string? TextoResposta2; 
+  public string? TextoResposta3; 
+  public int IdLevelResposta1; 
+  public int IdLevelResposta2; 
+  public int IdLevelResposta3; 
   public bool vcmorreu = false;
 }
